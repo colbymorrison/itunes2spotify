de83ca54b61fcb4a180e08828c69b658950f0ca4
Colby Morrison <colby.morrison@ibm.com>
2018-06-15 22:51:59 -0400

    get-itunes-album

--- itunes2spotify/itunes2spotify.py
+++ itunes2spotify/itunes2spotify.py
@@ -1,16 +1,37 @@
 import spotipy
 import spotipy.util as util
 import os
+import subprocess
+import time
+
 
 def spfy_sign_in(username):
+    global sp
     scope = 'user-library-modify'
 #    token = util.prompt_for_user_token(username, scope)
-    token = util.prompt_for_user_token(username, scope, client_id=os.environ['SPOTIPY_CLIENT_ID'], client_secret=os.environ['SPOTIPY_CLIENT_SECRET'], redirect_uri=os.environ['SPOTIPY_REDIRECT_URI'])
+    token = util.prompt_for_user_token(username, scope, client_id=os.environ['SPOTIPY_CLIENT_ID'],
+                                       client_secret=os.environ['SPOTIPY_CLIENT_SECRET'],
+                                       redirect_uri=os.environ['SPOTIPY_REDIRECT_URI'])
     if token:
         sp = spotipy.Spotify(auth=token)
     else:
         print("Error: couldn't access token")
-    results = sp.search(q="Ye", limit=1, type='album')
+
+
+def get_itunes_album():
+    itpath = "/opt/itunescli/build/Release/iTunescli"
+    process = subprocess.Popen([itpath, 'info'], stdout=subprocess.PIPE)
+    return process.communicate()[0]
+
+
+def add_to_lib():
+    results = sp.search(get_itunes_album(), type='album')
     print(results)
 
-spfy_sign_in("colbyacts@gmail.com")
+
+def main():
+    spfy_sign_in("colbyacts@gmail.com")
+    add_to_lib()
+
+if __name__ == "__main__":
+    main()
